f8fe3fc24f0ea8df1fc55f41f12f0d1ac24f682a
maehara08 <aso.riiiku08@gmail.com>
2017-08-13 15:06:24 +0900

    Reply bot.

--- fairy_line_bot.py
+++ fairy_line_bot.py
@@ -1,6 +1,39 @@
-from flask import Flask
+import os
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
+
+REPRY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
+
+LINE_HEADERS = {
+    'Content-type': 'application/json; charset=UTF-8',
+    'X-Line-ChannelID': os.environ['ChannelID'],
+    'X-Line-ChannelSecret': os.environ['ChannelSecret'],
+    'X-Line-Trusted-User-With-ACL': os.environ['MID']
+}
+
+
+def postMessage(replyToken, text):
+    textResponse = {'type': 'text', 'text': text}
+    reply = {'replyToken': replyToken, 'messages': textResponse}
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {}'.format(LINE_HEADERS['X-Line-Trusted-User-With-ACL'])
+    }
+    requests.post(REPRY_ENDPOINT, headers=headers, data=reply)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    events = request.json["events"]
+    for event in events:
+        replyToken = event["replyToken"]
+        if event['message']['type'] == 'text':
+            text = event['message']['type']['text']
+            postMessage(replyToken, text)
+
+    return ''
 
 
 @app.route('/')
